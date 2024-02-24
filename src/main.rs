2392b21d6b3115858748342af5d36f5e62653001
Lukasz Boldys <lukasz.boldys@gmail.com>
2024-02-24 10:17:31 +0000

    Semi working script

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,17 @@
 use clap::Parser;
 use requirements;
+use serde::Deserialize;
 use std::fs;
+
+#[derive(Deserialize, Debug)]
+struct Info {
+    version: String,
+}
+
+#[derive(Deserialize, Debug)]
+struct Response {
+    info: Info,
+}
 
 /// Simple check for new versions of python packages (pypi.org)
 #[derive(Parser, Debug)]
@@ -13,13 +24,24 @@ struct Args {
 
 fn main() {
     let args = Args::parse();
-    println!("Checking file {}", args.file_name);
+    // println!("Checking file {}", args.file_name);
 
     let contents =
         fs::read_to_string(args.file_name).expect("Should have been able to read the file");
     let reqs = requirements::parse_str(&contents).unwrap();
 
     for req in reqs.into_iter() {
-        println!("{:?}", req);
+        if req.name.is_some() {
+            let name = req.name.expect("Not a valid string").to_string();
+            let old_version = &req.specs[0].1;
+            let url = format!("https://pypi.org/pypi/{}/json", name);
+            let body = reqwest::blocking::get(url)
+                .expect("REASON")
+                .json::<Response>();
+            let new_version = body.unwrap().info.version;
+            if &new_version != old_version {
+                println!("{} {} {}", name, old_version, new_version);
+            }
+        }
     }
 }
