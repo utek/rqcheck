fab7cb1cc176bd3589974c9c19576170c96fe01a
Lukasz Boldys <lukasz.boldys@gmail.com>
2024-03-01 12:07:52 +0000

    Cleanup

--- src/main.rs
+++ src/main.rs
@@ -26,20 +26,26 @@ fn main() {
     let args = Args::parse();
     // println!("Checking file {}", args.file_name);
 
-    let contents =
+    let contents: String =
         fs::read_to_string(args.file_name).expect("Should have been able to read the file");
-    let reqs = requirements::parse_str(&contents).unwrap();
+    let reqs: Vec<requirements::Requirement<'_>> = requirements::parse_str(&contents).unwrap();
 
     for req in reqs.into_iter() {
         if req.name.is_some() {
-            let name = req.name.expect("Not a valid string").to_string();
-            let old_version = &req.specs[0].1;
-            let url = format!("https://pypi.org/pypi/{}/json", name);
-            let body = reqwest::blocking::get(url)
-                .expect("REASON")
+            let name: String = req.name.expect("Not a valid string").to_string();
+            let mut old_version: String = req.specs[0].1.clone();
+            if old_version.starts_with("v") {
+                old_version.remove(0);
+            }
+            let url: String = format!("https://pypi.org/pypi/{}/json", name);
+            let body: Result<Response, reqwest::Error> = reqwest::blocking::get(url)
+                .expect("Response from pypi.org")
                 .json::<Response>();
-            let new_version = body.unwrap().info.version;
-            if &new_version != old_version {
+            let mut new_version = body.unwrap().info.version;
+            if new_version.starts_with("v") {
+                new_version.remove(0);
+            }
+            if &new_version != &old_version {
                 println!("{} {} {}", name, old_version, new_version);
             }
         }
